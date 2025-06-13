0fb69a8319ccbd6001e88b6ad422f8405cd16cce
ksanguanshua <kondanai@gmail.com>
2025-06-13 17:25:12 -0400

    Update BoxBehaviour.cs

--- PRESSS/Assets/Scripts/BoxBehaviour.cs
+++ PRESSS/Assets/Scripts/BoxBehaviour.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class BoxBehaviour : MonoBehaviour
 {
+    GameObject player;
+
     public virtual void ClassUpdate()
+    {
+
+    }
+
+    void LaunchCheck()
     {
 
     }
